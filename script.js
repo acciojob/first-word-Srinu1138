368af23733e5870e820479249171101663122404
Srinu1138 <srinu6303571138@gmail.com>
2025-01-12 13:56:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
   // your code here
+let s = "see and stop";
+let n = s.length;
+let ans ='';
+for (let i=0; i<n; i++){
+    if(s[i]!=" "){
+        ans = ans+s[i];
+    }else{
+        break;
+    }
 }
-
+console.log(ans);
+}
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
